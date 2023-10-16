95c41f7bc76826c2998f349f0ae898a7a57811a8
Giovanna Midonz <midonz09@outlook.com>
2023-10-16 00:12:33 -0300

    Detona Raplph

---
+++ code/desafios/gerenciandoItensMagicos.js
@@ -0,0 +1,29 @@
+// Definição da classe ItemMagico
+class ItemMagico {
+  constructor(tipoItem, danoItem, resistenciaItem) {
+    this.tipo = tipoItem;
+    this.dano = danoItem;
+    this.resistencia = resistenciaItem;
+  }
+
+  calcularDano() {
+    return this.tipo === 'arma' ? this.dano * 2 : this.dano;
+  }
+}
+
+// Solicita ao usuário para digitar o tipo do item mágico, o dano no item e a resistência do item
+const tipoItem = gets();
+const danoItem = parseInt(gets());
+const resistenciaItem = parseInt(gets());
+
+// Cria um objeto ItemMagico personalizado com base no tipo escolhido
+const itemPersonalizado = new ItemMagico(tipoItem, danoItem, resistenciaItem);
+
+// Imprime os atributos do item personalizado
+print("Tipo: " + itemPersonalizado.tipo);
+print("Dano: " + itemPersonalizado.dano);
+print("Resistencia: " + itemPersonalizado.resistencia);
+
+// Calcula e imprime o dano causado pelo item personalizado em um combate simulado
+const danoTotal = itemPersonalizado.calcularDano();
+print("Dano em combate: " + danoTotal);
